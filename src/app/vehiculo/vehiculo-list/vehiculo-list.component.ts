f691127d533cff576484304649df1a9c1f6b6741
Patrick Mykoda <p.mykoda@uniandes.edu.co>
2022-05-01 14:33:25 -0500

    Punto No.3: Implementación de la prueba del componente Vehiculo-List

--- src/app/vehiculo/vehiculo-list/vehiculo-list.component.ts
+++ src/app/vehiculo/vehiculo-list/vehiculo-list.component.ts
@@ -10,6 +10,7 @@ import { VehiculoService } from '../vehiculo.service';
 export class VehiculoListComponent implements OnInit {
 
   vehiculos: Array<Vehiculo> = [];
+  vehiculosOrdenados: Array<string> = [];
 
   constructor(private vehiculoService: VehiculoService) { }
 
@@ -18,6 +19,16 @@ export class VehiculoListComponent implements OnInit {
       this.vehiculos = vehiculos;
     });
   }
+
+  /*listarMarcas(vehiculos: Array<Vehiculo>): void {
+    let vehiculosOrdenados = [];
+    let j = 0;
+    for(let i=0; i<vehiculos.length; i++){
+      if(vehiculos[i].marca )
+      vehiculosOrdenados[j] = vehiculos[i].marca;
+    }
+    this.vehiculosOrdenados = vehiculosOrdenados;
+  }*/
 
   ngOnInit() {
     this.getVehiculos();
